220852aefb6df5ee81f76a458e75c1fb1b038adf
Axure <axurez@vip.qq.com>
2015-09-08 07:06:04 +0800

    Initial project.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Forum Markup Tools
+
+A tool to process markup languages used within forums.
+
+Written in C++ with Boost.
---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<boost/tokenizer.hpp>
+#include <boost/spirit/include/lex_lexertl.hpp>
+#include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/spirit/include/phoenix_statement.hpp>
+#include <boost/spirit/include/phoenix_algorithm.hpp>
+#include <boost/spirit/include/phoenix_core.hpp>
+
+using namespace std;
+int main() {
+  cout << "Hello, World!" << endl;
+  return 0;
+}
